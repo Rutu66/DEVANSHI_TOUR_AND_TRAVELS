b3c2ad66269ac253c0659cfa16e87e9c01512ad2
Rutu66 <rutukotadiya999@gmail.com>
2024-07-25 22:13:50 +0530

    database

--- devanshi_tour_and_travels/settings.py
+++ devanshi_tour_and_travels/settings.py
@@ -81,17 +81,13 @@ WSGI_APPLICATION = 'devanshi_tour_and_travels.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
-        'HOST': config('DB_HOST', default='localhost'),
-        'PORT': config('DB_PORT', default='5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
 
 
 
